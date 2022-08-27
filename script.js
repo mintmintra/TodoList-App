de228eda1b29094ac44d48cda38e9facb1629410
mintmintra <mintracss019@gmail.com>
2022-08-28 01:03:02 +0700

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const taskInput = document.querySelector(".task-input input")
+let todos = JSON.parse(localStorage.getItem("todo-list"));
 taskInput.addEventListener("keyup", e => {
-    let userTask = taskInput.value;
-    if(e.key == "Enter"){
-        console.log(userTask)
+    let userTask = taskInput.value.trim();
+    if(e.key == "Enter" && userTask){
+       if(!todos){
+        todos = [];
+       }
+       taskInput.value = ""
+       let taskInfo = {name: userTask, status: "pending"}
+       todos.push(taskInfo)
+       localStorage.setItem("todo-list", JSON.stringify(todos));
+       showTodo();
     }
 })
