7cd4124f97c508de4047102075f103253b357656
mintmintra <mintracss019@gmail.com>
2022-08-28 00:27:48 +0700

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 const taskInput = document.querySelector(".task-input input")
+taskInput.addEventListener("keyup", e => {
+    let userTask = taskInput.value;
+    if(e.key == "Enter"){
+        console.log(userTask)
+    }
+})
