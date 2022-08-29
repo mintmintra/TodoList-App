485983fbfbbc3ec720ee2b7fea6a4b449bdb5433
mintmintra <mintracss019@gmail.com>
2022-08-29 23:34:57 +0700

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 const taskInput = document.querySelector(".task-input input")
+const taskBox = document.querySelector(".task-box")
+
 let todos = JSON.parse(localStorage.getItem("todo-list"));
+
+function showTodo(){
+    let li = "";
+    todos.forEach((todo, id) => {
+        li += `<li class="task">
+        <label for="${id}">
+            <input type="checkbox" id="${id}">
+            <p>${todo.name}</p>
+        </label>
+        <div class="settings">
+            <i class="uil uil-ellipsis-h"></i>
+            <ul class="task-menu">
+                <li><i class="uil uil-pen"></i>Edit</li>
+                <li><i class="uil uil-trash"></i>Delete</li>
+            </ul>
+        </div>
+    </li>`;
+    })
+}
+
+showTodo();
+
 taskInput.addEventListener("keyup", e => {
     let userTask = taskInput.value.trim();
     if(e.key == "Enter" && userTask){
